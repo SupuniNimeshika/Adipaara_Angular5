128433e9774803d9910dc64bd72243416d4d7fca
Your Name <supuni.nimeshika@gmail.com>
2018-02-20 14:41:22 +0530

    Connect Firebase Database to Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AlertModule } from 'ngx-bootstrap';
 
+import {AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule} from 'angularfire2/auth';
+
+import { environment } from './../environments/environment';
+
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +18,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AlertModule.forRoot()
+    AlertModule.forRoot(),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,11 @@
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyD8McbofDmAWHM0k_If029mI3y6GfX_YAg',
+    authDomain: 'adipaara-c44db.firebaseapp.com',
+    databaseURL: 'https://adipaara-c44db.firebaseio.com',
+    projectId: 'adipaara-c44db',
+    storageBucket: 'adipaara-c44db.appspot.com',
+    messagingSenderId: '477157590130'
+  }
 };
